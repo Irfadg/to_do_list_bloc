566018fa7965028ee00fcd35a846a543cba51ad7
Irfad G <irfad.g@direction.biz>
2024-01-07 15:04:50 +0530

    handleing error

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -7,9 +7,30 @@ class TodoCubit extends Cubit<List<Todo>> {
 
   void addToTodo(String title,DateTime date){
 
+    if(title.isEmpty){
+      addError('Titlr is empty');
+    }
+
     final todo = Todo(todoName: title, whenToDoCreated: date);
 
     emit([...state,todo]);
       
+  }
+  @override
+  void onChange(Change<List<Todo>> change) {
+    // TODO: implement onChange
+    super.onChange(change);
+
+    print('todo $change');
+
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+    super.onError(error, stackTrace);
+
+    print(error);
+
   }
 }
